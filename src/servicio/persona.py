eaadd77e08b89698045afbdb56402d4c1b9ab8b7
User <ug-fca@ug.edu.gob>
2025-06-05 11:51:40 -0500

    segundo commit

--- src/servicio/persona.py
+++ src/servicio/persona.py
@@ -9,6 +9,16 @@ class PersonaServicio(QMainWindow):
         self.ui = Ui_vtnPrincipal()
         self.ui.setupUi(self)
         self.ui.btnGuardar.clicked.connect(self.guardar)
+        self.ui.btnBorrar.clicked.connect(self.borrar)
 
     def guardar(self):
+        print(self.ui.txtNombre.text())
+        print(self.ui.txtApellido.text())
+        print(self.ui.txtCedula.text())
         print('Se hizo clic en el boton guardar')
+
+    def borrar(self):
+        self.ui.txtNombre.setText("")
+        self.ui.txtApellido.setText("")
+        self.ui.txtCedula.setText("")
+        print('Se hizo clic en el boton borrar')
