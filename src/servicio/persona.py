e62a342f5dcf24056e8826fe1b2a59db036e532b
User <ug-fca@ug.edu.gob>
2025-06-12 11:59:50 -0500

    actualizado

--- src/servicio/persona.py
+++ src/servicio/persona.py
@@ -1,5 +1,5 @@
 from PySide6.QtGui import QIntValidator
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtWidgets import QMainWindow, QMessageBox
 
 from src.UI.vtnPrincipal import Ui_vtnPrincipal
 
@@ -13,18 +13,6 @@ class PersonaServicio(QMainWindow):
         self.ui.btnBorrar.clicked.connect(self.borrar)
         self.ui.txtCedula.setValidator(QIntValidator())
 
-    #def guardar(self):
-        #if self.ui.txtNombre.text() == "" or self.ui.txtApellido.text() == "":
-            #print('Ingrese todos los datos.')
-        #else:
-            #print ('Se puede guardar.')
-            #print(self.ui.txtNombre.text())
-            #print(self.ui.txtApellido.text())
-           #print(self.ui.txtCedula.text())
-            #print(self.ui.txtemail.text())
-           #print(self.ui.cbsexo.currentText())
-            #print('Se hizo clic en el boton guardar')
-
     def guardar(self):
         if (self.ui.txtNombre.text() == "" or
                 self.ui.txtApellido.text() == "" or
@@ -32,13 +20,24 @@ class PersonaServicio(QMainWindow):
                 self.ui.txtEmail.text() == "" or
                 self.ui.cbSexo.currentText() not in ["Masculino", "Femenino"]):
             print('Ingrese todos los datos.')
-        else:
-            print('Se puede guardar.')
-            print(self.ui.txtNombre.text())
-            print(self.ui.txtApellido.text())
-            print(self.ui.txtCedula.text())
-            print(self.ui.txtEmail.text())
-            print(self.ui.cbSexo.currentText())
+            #QMessageBox.warning(self, "Advertencia", "Ingrese los datos.")
+            #QMessageBox.critical(self, "Advertencia", "Ingrese los datos.")
+            QMessageBox.information(self, "Advertencia", "Ingrese los datos.")
+            return
+
+        if len(self.ui.txtCedula.text()) != 10:
+            print("La cédula debe tener exactamente 10 dígitos.")
+            return
+
+        #print('Se puede guardar.')
+        #QMessageBox.information(self, "Información", "Se guardó información.")
+        self.ui.statusbar.showMessage('Se guardó la información', 3000)
+        print(self.ui.txtNombre.text())
+        print(self.ui.txtApellido.text())
+        print(self.ui.txtCedula.text())
+        print(self.ui.txtEmail.text())
+        print(self.ui.cbSexo.currentText())
+        self.borrar()
 
     def borrar(self):
         self.ui.txtNombre.setText("")
@@ -47,3 +46,4 @@ class PersonaServicio(QMainWindow):
         self.ui.txtEmail.setText("")
         self.ui.cbSexo.setCurrentText("Seleccionar")
         print('Se hizo clic en el boton borrar')
+
